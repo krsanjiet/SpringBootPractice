9e81c0907cf2d92a75bb197a82bb64af92974f0e
krsanjiet <krsanjiet91@gmail.com>
2022-02-28 01:32:48 +0530

    initial commit

---
+++ src/main/java/com/sanjeet/Spring/boot/tutorial/controller/HelloController.java
@@ -0,0 +1,13 @@
+package com.sanjeet.Spring.boot.tutorial.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/")
+    public String helloWorld(){
+        return "Welcome to My Page, Sanjeet";
+    }
+}
